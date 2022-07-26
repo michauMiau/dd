bee86fbc85683f3e86bd3a51aeb886dbf890234d
michauMiau <64422239+michauMiau@users.noreply.github.com>
2022-07-26 10:13:49 +0200

    audiofixe

--- nyancat/script/audio.js
+++ nyancat/script/audio.js
@@ -1,10 +1,19 @@
+
+function loop () {
+
+
 checkBox = document.getElementById('play');
 if(checkBox.checked) {
 
-    var audio = new Audio("nyancat.mp3");
+   
 
     audio.play();
 }
+
 else{
     audio.pause();
 }
+
+}
+
+loop();
